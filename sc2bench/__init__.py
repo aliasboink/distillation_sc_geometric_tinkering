3f6e20ed2d8ca6d27fcec35ca540063b6c7505df
banciuadrian <scoalaadilaur@gmail.com>
2022-05-19 19:47:54 +0300

    Removed sc2bench and tinkered with some things.

--- sc2bench/__init__.py
+++
@@ -1,3 +0,0 @@
-from . import models, transforms, loss
-
-__version__ = '0.0.3-dev'
--- sc2bench/common/config_util.py
+++
@@ -1,9 +0,0 @@
-def overwrite_config(org_config, sub_config):
-    for sub_key, sub_value in sub_config.items():
-        if sub_key in org_config:
-            if isinstance(sub_value, dict):
-                overwrite_config(org_config[sub_key], sub_value)
-            else:
-                org_config[sub_key] = sub_value
-        else:
-            org_config[sub_key] = sub_value
